e6b4cfa2b14fea98053db342c2b1377b5f2edf56
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2015-01-20 16:02:35 -0500

    Initial Commit

---
+++ helicopter_lander/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helicopter_lander
+//
+//  Created by ScottStubblebine on 1/20/15.
+//  Copyright (c) 2015 ScottStubblebine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helicopter_lander/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  helicopter_lander
+//
+//  Created by ScottStubblebine on 1/20/15.
+//  Copyright (c) 2015 ScottStubblebine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
